5720f99448918008bad0314a875ba5bb09438292
안수진 <asj7090@gmail.com>
2024-08-06 19:14:33 +0900

    [week3_안수진] 배열 합치기 제출

--- 투포인터/배열합치기-11728/ssuzyn.java
+++ 투포인터/배열합치기-11728/ssuzyn.java
@@ -11,50 +11,47 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken()); // 배열 A의 크기
         int M = Integer.parseInt(st.nextToken()); // 배열 B의 크기
-
-        // A 배열 생성 및 초기화
         int[] A = new int[N];
+        int[] B = new int[M];
+
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        // B 배열 생성 및 초기화
-        int[] B = new int[M];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
             B[i] = Integer.parseInt(st.nextToken());
         }
 
-        int pA = 0; // A 배열 포인터
-        int pB = 0; // B 배열 포인터
+
+        int idxA = 0; // 배열 A의 인덱스
+        int idxB = 0; // 배열 B의 인덱스
+        int idxTmp = 0; // 병합 배열의 인덱스
+        int [] merged = new int[N + M];
+
+        while(idxA < N && idxB < M) {
+            if(A[idxA] > B[idxB]) {
+                merged[idxTmp++] = B[idxB++];
+            }
+            else{
+                merged[idxTmp++] = A[idxA++];
+            }
+        }
+
+        while(idxA < N) { // 배열 A의 나머지 요소 병합 배열에 할당
+            merged[idxTmp++] = A[idxA++];
+        }
+
+        while(idxB < M) { // 배열 B의 나머지 요소 병합 배열에 할당
+            merged[idxTmp++] = B[idxB++];
+        }
 
         StringBuilder sb = new StringBuilder();
-
-        // A, B 중 포인터가 한 배열의 끝을 가리킬 때까지 반복
-        while(pA < N && pB < M) {
-            // A, B 배열에서 현재 가리키는 원소가 작은 것부터 ans에 담아줌
-            if(A[pA] < B[pB]) {
-                sb.append(A[pA++] + " ");
-            }
-            else {
-                sb.append(B[pB++] + " ");
-            }
+        for(int num : merged) {
+            sb.append(num).append(" ");
         }
-
-        if(pA == N) { // B의 원소가 남은 경우
-            // 주어진 배열은 정렬되어 있기 때문에 순서대로 담아줌
-            for(int i = pB; i < M; i++) {
-                sb.append(B[i] + " ");
-            }
-        }
-        else if(pB == M) { // A의 원소가 남은 경우도 동일하게 작성
-            for(int i = pA; i < N; i++) {
-                sb.append(A[i] + " ");
-            }
-        }
-
-        System.out.println(sb); // 결과 출력
+        System.out.println(sb);
 
     }
 
